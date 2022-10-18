284fa25170149425141c6e1b44c431728c84b444
AP Java Period 5 <apjavaperiod5@ac1mwk17njc9h.78b505.nycboe.org>
2022-10-18 12:07:05 -0400

    catch up commit

--- 06-username/UserName.java
+++ 06-username/UserName.java
@@ -7,11 +7,17 @@ public class UserName {
     Scanner s = new Scanner(System.in);
     Random rand = new Random();
     
-    int rand_int1 =rand.nextInt(10);
-    int rand_int2 =rand.nextInt(10);
-    int rand_int3 =rand.nextInt(10);
-    int rand_int4 =rand.nextInt(10);
-
+    //int rand_int1 =rand.nextInt(10);
+    //int rand_int2 =rand.nextInt(10);
+    //int rand_int3 =rand.nextInt(10);
+    //int rand_int4 =rand.nextInt(10);
+    //int max = 130;
+    //int min = 22;
+    //Random randomNum = new Random();
+    //int random = min + randomNum.nextInt(max);
+    
+    
+    
     // get String input from user
     System.out.print("Enter your first name: ");
     String firstName = s.nextLine();
@@ -29,23 +35,35 @@ public class UserName {
         System.out.println(firstName + lastName.substring(0, 1) + Number + "@nycstudents.net");
         
     }
-    System.out.print("Your password is " + rand_int1 + rand_int2 + rand_int3 + rand_int4);
-    
-    // test output
-    //System.out.println("Hello " + initialize(firstName) + "." + initialize(lastName) + ".");
-    
-
+    //System.out.print("Your password is " + rand_int1 + rand_int2 + rand_int3 + rand_int4);{
+    genartePassword(8);
     s.close();
-  } // end main method
-
-  /*
+    }  
+    public static String genartePassword(int length) {
+    String password = "";
+    
+    
+    
+        
+    int rand = (char)(int)(Math.random()*(90-65+1) + 65);
+    char c = (char)rand;
+    System.out.print(rand + " " + c);
+        
+        
+        
+    return password;    
+    }
+        
+        // test output
+    //System.out.println("Hello " + initialize(firstName) + "." + initialize(lastName) + ".");
+   /*
    * Name: initialize
    * Purpose: send back the first character (inital) of a name
    * Input: a name (String)
    * Return: a single character (String)
    */
-  public static String initialize(String n) {
+    public static String initialize(String n) {
     return n.substring(0, 1);
-  } // end initialize method
+    } // end initialize method
 
 } // end class
